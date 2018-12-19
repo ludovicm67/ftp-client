746c729899407eb00d1e68f77ba10039ca168c3c
ludovicm67 <ludovicmuller1@gmail.com>
2018-12-19 17:41:19 +0100

    started to work with sockets

--- network.c
+++ network.c
@@ -32,7 +32,34 @@ void fetch_addr_infos(char *host) {
 }
 
 void init_sockets() {
+  struct sockaddr * s;
+  struct sockaddr_in s4;
+  struct sockaddr_in6 s6;
+  int len;
   int control_fd = socket(ftp_state.infos->ai_family, SOCK_STREAM, 0);
+
+  if (ftp_state.infos->ai_family == AF_INET6) {
+    s6.sin6_family = AF_INET6;
+    s6.sin6_addr = in6addr_any;
+    s6.sin6_port = htons(0);
+    s6.sin6_flowinfo = 0;
+    s = (struct sockaddr *) &s6;
+    len = sizeof(struct sockaddr_in6);
+  } else {
+    s4.sin_family = AF_INET;
+    s4.sin_addr.s_addr = INADDR_ANY;
+    s4.sin_port = htons(0);
+    s = (struct sockaddr *) &s4;
+    len = sizeof(struct sockaddr_in);
+  }
+
+  if (bind(control_fd, s, len) < 0) {
+    perror("bind");
+    return;
+  }
+
+  listen(control_fd, 5);
+
   // @TODO: do something
   close(control_fd);
 }
